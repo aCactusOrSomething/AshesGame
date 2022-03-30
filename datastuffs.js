e579a032e02d5eeabcef9f04df78266bc7dc2386
aCactusOrSomething <31144053+aCactusOrSomething@users.noreply.github.com>
2022-03-29 20:33:10 -0500

    lets see if this fixes things

--- datastuffs.js
+++ datastuffs.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize');
+const sqlite3 = require('sqlite3');
 
+new sqlite3.Database('database.sqlite');
 
-const sequelize = new Sequelize('database', 'user', 'password', {
+const sequelize = new Sequelize('db', 'user', 'password', {
 	host: 'localhost',
 	dialect: 'sqlite',
 	dialectOptions: {
@@ -9,7 +11,7 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 	},
 	logging: false,
 	// SQLite only
-	storage: 'database.sqlite',
+	// storage: 'database.sqlite',
 });
 
 function makeSession(id) {
